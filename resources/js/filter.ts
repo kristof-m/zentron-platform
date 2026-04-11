91a29257ca62a07dc0faa7ba0b18786e30861ccb
Teodor Potancok <teodor.potancok@gmail.com>
2026-04-11 16:53:18 +0200

    Replace history when changing filters

--- resources/js/filter.ts
+++ resources/js/filter.ts
@@ -1,10 +1,17 @@
 const formElement: HTMLFormElement | null = document.querySelector(".catalog-controls");
 
 if (formElement) {
+    formElement.addEventListener('submit', e => {
+        e.preventDefault();
+        // @ts-ignore TS2345: type definition is wrong
+        const params = new URLSearchParams(new FormData(e.currentTarget));
+        location.replace(`${window.location.pathname}?${params.toString()}`);
+    });
+
     for (const node of document.querySelectorAll(".catalog-controls input, .catalog-controls select")) {
         if (node instanceof HTMLInputElement || node instanceof HTMLSelectElement) {
             node.addEventListener('change', () => {
-                formElement.submit();
+                formElement.requestSubmit();
             })
         }
     }
