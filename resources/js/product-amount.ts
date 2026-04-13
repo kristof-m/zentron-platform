196045f3f1729b055b4b6d23631f1142c21247ae
Majky123452 <michaelkristof123@gmail.com>
2026-04-13 18:07:19 +0200

    Enhance product button behaviour

--- resources/js/product-amount.ts
+++ resources/js/product-amount.ts
@@ -1,12 +1,26 @@
 const amountField = document.getElementById("amount-field");
+const minusBtn = document.getElementById("amount-minus");
+const plusBtn = document.getElementById("amount-plus");
 
-if (amountField instanceof HTMLInputElement) {
-    document.getElementById("amount-plus")?.addEventListener('click', () => {
-        amountField.value = (Number.parseInt(amountField.value) + 1).toString();
-    });
-    document.getElementById("amount-minus")?.addEventListener('click', () => {
-        if (Number.parseInt(amountField.value) >= 2) {
-            amountField.value = (Number.parseInt(amountField.value) - 1).toString();
-        }
-    });
+if (amountField instanceof HTMLInputElement && minusBtn instanceof HTMLButtonElement && plusBtn instanceof HTMLButtonElement){
+  const syncButtons = () => {
+    const amount = Number.parseInt(amountField.value || "1");
+    if (amount < 1) amountField.value = "1";
+    minusBtn.disabled = (Number.parseInt(amountField.value)) <= 1;
+  };
+
+  plusBtn.addEventListener("click", () => {
+    amountField.value = (Number.parseInt(amountField.value) + 1).toString();
+    syncButtons();
+  });
+
+  minusBtn.addEventListener("click", () => {
+    if (!minusBtn.disabled) {
+      amountField.value = (Number.parseInt(amountField.value) - 1).toString();
+      syncButtons();
+    }
+  });
+
+  amountField.addEventListener("input", syncButtons);
+  syncButtons();
 }
