8341911698cbf3ef70b4baaea1ed2ca865e22d9a
Majky123452 <michaelkristof123@gmail.com>
2026-04-13 18:21:22 +0200

    Remove unused import

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Http\Filters\ControllerWithProducts;
-use App\Models\Brand;
 use App\Models\Product;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Http\Request;
