7c865b86662d37c17f89165187c42e731e48462a
Teodor Potancok <teodor.potancok@gmail.com>
2026-04-28 16:07:18 +0200

    Fix spacing on admin home

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -5,18 +5,19 @@
     @vite('resources/css/style.css')
     @vite('resources/css/admin.css')
     @vite('resources/css/auth.css')
+    @vite('resources/css/menu.css')
 </head>
 
 <body class="admin-page">
 
 @include('admin.header')
 
-<main>
+<main class="main">
     <h1>Hi, {{ $user->name }}</h1>
     <p>You are an admin!</p>
-    <a class="register-btn" href="{{ route('admin.products') }}">Manage Products</a>
-    <a class="register-btn" href="{{ route('product.new') }}">Add new product</a>
-    <a class="register-btn" href="{{ route('brand.new') }}">Add new brand</a>
+    <a class="menu-btn" href="{{ route('admin.products') }}">Manage Products</a>
+    <a class="menu-btn" href="{{ route('product.new') }}">Add new product</a>
+    <a class="menu-btn" href="{{ route('brand.new') }}">Add new brand</a>
     <form action="/logout" method="post">
         @csrf
         <button type="submit" class="register-btn">
