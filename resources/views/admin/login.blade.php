8ed652b1c41a7e8489f7b26506d88a8114209b4c
Majky123452 <michaelkristof123@gmail.com>
2026-05-01 12:48:35 +0200

    Add CSRF token to admin login form

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -31,7 +31,7 @@
             aria-label="Admin login form"
         >
             <h2>Sign in to admin panel</h2>
-
+            @csrf
             <form class="register-form" action="/login" method="post">
                 <input type="hidden" name="redirect-to" value="admin-home"/>
                 <label for="admin-email">E-mail</label>
