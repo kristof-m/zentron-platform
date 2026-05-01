49c29efd2affbd6692d64e0376b6e5b505f3f64c
Majky123452 <michaelkristof123@gmail.com>
2026-05-01 11:34:07 +0200

    Cleanup controller redundancy

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Http\Filters\ControllerWithProducts;
 use App\Models\Category;
-use App\Models\Product;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
 
